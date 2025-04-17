57fa434b98a238929907672365869e5947cb4b95
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-17 13:18:30 +0200

    add logger

--- src/main/scala/com/goamegah/trafficmonitor/db/DBSchemaManager.scala
+++ src/main/scala/com/goamegah/trafficmonitor/db/DBSchemaManager.scala
@@ -1,40 +1,76 @@
 package com.goamegah.trafficmonitor.db
 
 import java.nio.file.{Files, Paths}
+import java.sql.{Connection, DriverManager}
 import scala.io.Source
-import java.sql.DriverManager
+import java.io.File
 import com.goamegah.trafficmonitor.config.AppConfig
+import com.goamegah.trafficmonitor.load.PostgresLoader.getClass
+import org.slf4j.LoggerFactory
 
 object DBSchemaManager {
+  private val logger = LoggerFactory.getLogger(getClass)
 
-  def executeSqlFile(filePath: String): Unit = {
-    val sql = Source.fromFile(filePath).getLines().mkString("\n")
-    val conn = DriverManager.getConnection(
+  private def getConnection(): Connection = {
+    DriverManager.getConnection(
       AppConfig.Postgres.jdbcUrl,
       AppConfig.Postgres.user,
       AppConfig.Postgres.password
     )
+  }
 
+  private def executeSqlFile(path: String)(implicit conn: Connection): Unit = {
+    val file = new File(path)
+    if (!file.exists()) {
+      logger.warn(s"[WARN] Le fichier $path n’existe pas. Ignoré.")
+      return
+    }
+
+    val sql = Source.fromFile(file).getLines().mkString("\n")
     try {
       val stmt = conn.createStatement()
       stmt.execute(sql)
-      println(s"[OK] Script exécuté : $filePath")
+      logger.info(s"[OK] Script exécuté avec succès : ${file.getName}")
     } catch {
       case ex: Exception =>
-        println(s"[/!\\] Erreur d’exécution pour $filePath : ${ex.getMessage}")
+        logger.warn(s"[/!\\] Erreur lors de l'exécution du script ${file.getName} : ${ex.getMessage}")
+    }
+  }
+
+  def init(): Unit = {
+    logger.info("[...] Initialisation du schéma PostgreSQL...")
+
+    implicit val conn: Connection = getConnection()
+
+    try {
+      // Exécution des tables
+      executeSqlFile("ddl/tables/create_road_traffic_stats_minute.sql")
+      executeSqlFile("ddl/tables/create_road_traffic_stats_hour.sql")
+      executeSqlFile("ddl/tables/create_road_traffic_stats_sliding_window.sql")
+
+      // Exécution des vues
+      val viewDir = new File("ddl/views/")
+      if (viewDir.exists && viewDir.isDirectory) {
+        val viewFiles = viewDir.listFiles().filter(_.getName.endsWith(".sql")).sorted
+        viewFiles.foreach(view => executeSqlFile(view.getPath))
+      } else {
+        logger.warn("[WARN] Le dossier 'ddl/views/' est introuvable.")
+      }
+
+      logger.warn("[#] Schéma PostgreSQL initialisé.")
     } finally {
       conn.close()
     }
   }
 
-  def init(): Unit = {
-    println("[...] Initialisation du schéma PostgreSQL...")
-
-    executeSqlFile("ddl/tables/create_traffic_stream.sql")
-    executeSqlFile("ddl/views/create_view_traffic_status_latest.sql")
-    executeSqlFile("ddl/views/create_view_traffic_stats_per_minute.sql")
-
-    println("[#] Schéma PostgreSQL initialisé avec succès.")
+  def cleanup(): Unit = {
+    println("[...] Nettoyage du schéma PostgreSQL...")
+    implicit val conn: Connection = getConnection()
+    try {
+      executeSqlFile("ddl/cleanup/drop_all.sql")
+      logger.info("[OK] Schéma PostgreSQL nettoyé.")
+    } finally {
+      conn.close()
+    }
   }
 }
-
