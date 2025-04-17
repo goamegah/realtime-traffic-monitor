87ccf7c2c582dd88ba1152d128061c2d30b79c3b
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-17 13:14:33 +0200

    update mainApp

--- src/main/scala/com/goamegah/trafficmonitor/MainApp.scala
+++ src/main/scala/com/goamegah/trafficmonitor/MainApp.scala
@@ -5,11 +5,24 @@ import com.goamegah.trafficmonitor.streaming.TrafficStreamProcessor
 
 object MainApp {
   def main(args: Array[String]): Unit = {
+
     // Initialisation du schéma PostgreSQL
     DBSchemaManager.init()
 
-    // Lancement du streaming des données
+    // Démarrage du streaming
     TrafficStreamProcessor.start()
+
+    // Arrêt du streaming après 5 minutes
+    // Thread.sleep(5 * 60 * 1000) // 5 minutes
+
+    // Arrêt du streaming
+    // TrafficStreamProcessor.stop()
+
+    // Nettoyage du schéma PostgreSQL
+    // DBSchemaManager.cleanup()
+
+    // Arrêt de l'application
+    // spark.stop()
+
   }
 }
-
