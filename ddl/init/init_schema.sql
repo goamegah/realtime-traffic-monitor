f53460e1fc9b34c845d95212df7bcf0127be3abf
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-17 13:22:37 +0200

    update ddl

--- ddl/init/init_schema.sql
+++ ddl/init/init_schema.sql
@@ -1,5 +1,9 @@
 -- Création des tables
-\i ../tables/create_traffic_stream.sql
+\i ../tables/create_road_traffic_stats_minute.sql
+\i ../tables/create_road_traffic_stats_hour.sql
+\i ../tables/create_road_traffic_stats_sliding_window.sql
 
 -- Création des vues
-\i ../views/create_view_traffic_status_latest.sql
+\i ../views/view_traffic_evolution_by_road_name_hour.sql
+\i ../views/view_traffic_evolution_by_road_name_minute.sql
+
--- ddl/views/create_view_traffic_predictions.sql
+++
@@ -1,9 +0,0 @@
--- Prévue pour plus tard, quand on aura des prédictions ML
-CREATE OR REPLACE VIEW traffic_predictions AS
-SELECT
-    f.datetime,
-    f.location_id,
-    f.avg_speed,
-    p.predicted_status
-FROM traffic_features_for_prediction f
-         JOIN prediction_results p ON f.location_id = p.location_id AND f.datetime = p.datetime;
