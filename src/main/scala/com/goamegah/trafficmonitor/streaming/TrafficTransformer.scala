3f0edf0bfcf62a427c4d265df9ddf5faf8574645
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-17 13:18:13 +0200

    rename columns using snake case

--- src/main/scala/com/goamegah/trafficmonitor/streaming/TrafficTransformer.scala
+++ src/main/scala/com/goamegah/trafficmonitor/streaming/TrafficTransformer.scala
@@ -10,8 +10,6 @@ object TrafficTransformer {
 
     println("[#] Étape 1 - Données brutes (streaming)")
     inputDF.printSchema()
-    // /!\ .count() interdit en streaming
-    // /!\ .show() interdit en streaming
 
     // Étape 2 - Explosion du champ `results`
     val explodedDF = inputDF
@@ -27,14 +25,27 @@ object TrafficTransformer {
     println("[#] Étape 3 - Après sélection des champs")
     selectedDF.printSchema()
 
-    // Étape 4 - Transformation : ajout lat/lon et filtrage
+    // Étape 4 - Transformation finale
     val transformedDF = selectedDF
       .withColumn("lat", col("geo_point_2d.lat"))
       .withColumn("lon", col("geo_point_2d.lon"))
+      .withColumn("geometry_linestring", to_json(col("geo_shape.geometry")).cast("string"))
       .drop("geo_point_2d")
+      .drop("geo_shape")
+      .withColumn("datetime", to_timestamp(col("datetime")))
+      .withColumnRenamed("predefinedlocationreference", "location_id")
+      .withColumnRenamed("averagevehiclespeed", "average_speed")
+      .withColumnRenamed("traveltime", "travel_time")
+      .withColumnRenamed("traveltimereliability", "travel_time_reliability")
+      .withColumnRenamed("trafficstatus", "traffic_status")
+      .withColumnRenamed("vehicleprobemeasurement", "vehicle_probe")
+      .withColumnRenamed("denomination", "road_name")
+      .withColumnRenamed("vitesse_maxi", "max_speed")
+      .withColumnRenamed("hierarchie", "road_category")
       .filter(col("datetime").isNotNull)
 
-    println("[#] Étape 4 - Après nettoyage/filtrage final")
+
+    println("[#] Étape 4 - Après nettoyage, cast et renommage")
     transformedDF.printSchema()
 
     transformedDF
