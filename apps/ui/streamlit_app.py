ac1bd5d2db951807d644a3d7df6cc3a74c3e1da5
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-18 12:24:36 +0200

    add main streamlit apps

---
+++ apps/ui/streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Traffic Dashboard", layout="wide")
+st.title("🚦 Bienvenue sur la plateforme TrafficWatch")
+st.markdown("Utilisez le menu à gauche pour naviguer entre les pages.")
