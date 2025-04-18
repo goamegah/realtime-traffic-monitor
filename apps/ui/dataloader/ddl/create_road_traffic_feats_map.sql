3cf0b74ef02df3ee63bab10e0d66570745f7f59d
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-18 12:18:52 +0200

    add ddl

---
+++ apps/ui/dataloader/ddl/create_road_traffic_feats_map.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS road_traffic_feats_map (
+    timestamp TIMESTAMP,
+    period TIMESTAMP,
+    location_id TEXT,
+    road_name TEXT,
+    road_category TEXT,
+    traffic_status TEXT
+);
---
+++ apps/ui/dataloader/ddl/create_road_traffic_stats_hour.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS road_traffic_stats_hour (
+                                                     period TIMESTAMP,
+                                                     num_troncon INTEGER,
+                                                     total_vehicle_probe INTEGER,
+                                                     average_speed DOUBLE PRECISION,
+                                                     average_travel_time DOUBLE PRECISION,
+                                                     average_travel_time_reliability DOUBLE PRECISION,
+                                                     road_name TEXT,
+                                                     max_speed INTEGER,
+                                                     traffic_status_list TEXT[]
+);
