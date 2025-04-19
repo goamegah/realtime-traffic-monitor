1c0af9dd6e0418e471bf930793bc3162f4608e3d
goamegah <komlan.godwin.amegah@gmail.com>
2025-04-20 01:09:51 +0200

    update

--- ddl/cleanup/drop_all.sql
+++ ddl/cleanup/drop_all.sql
@@ -1,12 +1,10 @@
 -- Supprimer les vues si elles existent
-DROP VIEW IF EXISTS traffic_status_latest CASCADE;
-DROP VIEW IF EXISTS traffic_stats_per_minute_summary CASCADE;
-DROP VIEW IF EXISTS traffic_stats_hourly_summary CASCADE;
-DROP VIEW IF EXISTS traffic_sliding_window_summary CASCADE;
+DROP VIEW IF EXISTS traffic_evolution_by_road_name_hour CASCADE;
+DROP VIEW IF EXISTS traffic_evolution_by_road_name CASCADE;
 
 -- Supprimer la table principale
-DROP TABLE IF EXISTS traffic_stream CASCADE;
-DROP TABLE IF EXISTS traffic_stats_by_minute CASCADE;
-DROP TABLE IF EXISTS traffic_stats_by_hour CASCADE;
-DROP TABLE IF EXISTS traffic_stats_sliding_window CASCADE;
+DROP TABLE IF EXISTS road_traffic_feats_map CASCADE;
+DROP TABLE IF EXISTS road_traffic_stats_hour CASCADE;
+DROP TABLE IF EXISTS road_traffic_stats_minute CASCADE;
+DROP TABLE IF EXISTS road_traffic_stats_sliding_window CASCADE;
 
